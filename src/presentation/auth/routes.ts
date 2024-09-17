96dad8e064a2c4f229d63ba02b297f80c8903c4f
Stefano Palomino <stefanop21@outlook.es>
2024-09-17 16:49:23 -0500

    feat(auth): added auth controller

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
+import { AuthController } from './controller';
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
+    const controller = new AuthController();
 
-    router.post('/login', (req, res) => {
-      res.json('Login');
-    });
+    router.post('/login', controller.loginUser);
+    router.post('/register', controller.registerUser);
 
     return router;
   }
