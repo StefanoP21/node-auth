46dd171ec9424d94e42c07c1d6150e4b0f02df9c
Stefano Palomino <stefanop21@outlook.es>
2024-09-18 18:10:15 -0500

    feat(presentation): added repository in routes

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { AuthController } from './controller';
+import { AuthDatasourceImpl, AuthRepositoryImpl } from '../../infraestructure';
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
-    const controller = new AuthController();
+    const datasource = new AuthDatasourceImpl();
+    const repository = new AuthRepositoryImpl(datasource);
+    const controller = new AuthController(repository);
 
     router.post('/login', controller.loginUser);
     router.post('/register', controller.registerUser);
