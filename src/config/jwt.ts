136caba08498bef9efb3afc03b43db2b439473a2
Stefano Palomino <stefanop21@outlook.es>
2024-09-23 18:13:57 -0500

    feat(config): added jwt secret

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,4 +1,7 @@
 import jwt from 'jsonwebtoken';
+import { envs } from './envs';
+
+const JWT_SECRET = envs.JWT_SECRET;
 
 export class JwtAdapter {
   static async generateToken(
@@ -6,7 +9,7 @@ export class JwtAdapter {
     duration: string = '2h'
   ) {
     return new Promise((resolve) => {
-      jwt.sign(payload, 'SEED', { expiresIn: duration }, (err, token) => {
+      jwt.sign(payload, JWT_SECRET, { expiresIn: duration }, (err, token) => {
         if (err) {
           console.error(err);
           throw new Error('Error generating token');
@@ -19,7 +22,7 @@ export class JwtAdapter {
 
   static async verifyToken<T>(token: string): Promise<T> {
     return new Promise((resolve) => {
-      jwt.verify(token, 'SEED', (err, token) => {
+      jwt.verify(token, JWT_SECRET, (err, token) => {
         if (err) {
           console.error(err);
           throw new Error('Error verifying token');
